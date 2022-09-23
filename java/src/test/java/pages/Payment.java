ad0e8e3ada410469a812383a3c5bb35abbfdaa9b
Angie Pineda <Angie.Pineda@endava.com>
2022-09-23 06:34:26 -0500

    Tc add 2 items to cart and then delete one and follow the payment

--- java/src/test/java/pages/Payment.java
+++ java/src/test/java/pages/Payment.java
@@ -2,9 +2,11 @@ package pages;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
+import io.qameta.allure.Step;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pages.components.CartNavigation;
+import pages.components.CartSummary;
 
 public class Payment extends BasePage {
 
@@ -15,20 +17,27 @@ public class Payment extends BasePage {
     public Locator successMessage;
     private final Logger log = LoggerFactory.getLogger(Payment.class);
 
+    public CartSummary cartSummary;
+
     public Payment(Page page) {
         super(page);
         cartNavigation = new CartNavigation(page);
         payByBank = page.locator(".bankwire");
         confirmMyOrderButton = page.locator("//*[@id=\"cart_navigation\"]/button");
         successMessage = page.locator(".cheque-indent");
+        cartSummary = new CartSummary(page);
     }
 
+    @Step("Click on pay by bank")
     public void clickOnPayByBank() {
+        payByBank.hover();
         payByBank.click();
         log.info("Click on pay by bank");
     }
 
+    @Step("Click on confirm my order button")
     public void clickOnConfirmMyOrderButton() {
+        confirmMyOrderButton.hover();
         confirmMyOrderButton.click();
         log.info("Click on confirm my order button");
     }
--- java/src/test/java/pages/Submit.java
+++ java/src/test/java/pages/Submit.java
@@ -2,13 +2,16 @@ package pages;
 
 import com.microsoft.playwright.Page;
 import pages.components.CartNavigation;
+import pages.components.CartSummary;
 
-public class Submit extends BasePage{
+public class Submit extends BasePage {
 
     public CartNavigation cartNavigation;
+    public CartSummary cartSummary;
 
     public Submit(Page page) {
         super(page);
         cartNavigation = new CartNavigation(page);
+        cartSummary = new CartSummary(page);
     }
 }
