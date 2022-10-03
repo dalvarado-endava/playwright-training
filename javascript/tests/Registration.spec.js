6eebeff456512720bae3778dd85a553d4c5fb344
Juan Fernando Saenz Sanchez <JuanFernando.SaenzSanchez@endava.com>
2022-10-02 21:46:14 -0500

    javascript: Added test steps for Registration test cases.

--- javascript/tests/Registration.spec.js
+++ javascript/tests/Registration.spec.js
@@ -26,30 +26,73 @@ test.describe('Registration', () => {
         await header.clickSignInButton();
     });
 
+
     test('Successful user registration', async ({ page }) => {
-        await authenticationPage.fillEmail(randomize.email());
-        await authenticationPage.clickCreateAnAccountButton();
-        await accountCreationPage.validatePageTitle(data.titles.loginpage);
-        await accountCreationPage.fillCustomerFirstName(data.user.firstname)
-        await accountCreationPage.fillCustomerLastName(data.user.lastname)
-        await accountCreationPage.fillPassword(process.env.PASSWORD)
-        await accountCreationPage.fillFirstName(data.user.firstname)
-        await accountCreationPage.fillLastName(data.user.lastname)
-        await accountCreationPage.fillAddress(data.user.address)
-        await accountCreationPage.fillCity(data.user.city)
-        await accountCreationPage.fillState(randomize.state())
-        await accountCreationPage.fillPostCode(data.user.postcode)
-        await accountCreationPage.fillMobilePhone(data.user.mobilephone)
-        await accountCreationPage.clickRegisterButton();
-        await myAccountPage.validateMyAccountTitle(data.titles.myaccountpage);
-        await myAccountPage.validateMyAccountText(data.texts.myaccountpageH1);
-        await header.validateRandomUserName(data.user.firstname, data.user.lastname);
+
+        await test.step('Fill email field', async () =>
+            await authenticationPage.fillEmail(randomize.email()))
+
+        await test.step('Click "Create an account" button', async () =>
+            await authenticationPage.clickCreateAnAccountButton())
+
+        await test.step('Validate page title', async () =>
+            await accountCreationPage.validatePageTitle(data.titles.loginpage))
+
+        await test.step('Fill customer firstname field', async () =>
+            await accountCreationPage.fillCustomerFirstName(data.user.firstname))
+
+        await test.step('Fill customer lastname field', async () =>
+            await accountCreationPage.fillCustomerLastName(data.user.lastname))
+
+        await test.step('Fill password field', async () =>
+            await accountCreationPage.fillPassword(process.env.PASSWORD))
+
+        await test.step('Fill firstname field', async () =>
+            await accountCreationPage.fillFirstName(data.user.firstname))
+
+        await test.step('Fill lastname field', async () =>
+            await accountCreationPage.fillLastName(data.user.lastname))
+
+        await test.step('Fill address field', async () =>
+            await accountCreationPage.fillAddress(data.user.address))
+
+        await test.step('Fill city field', async () =>
+            await accountCreationPage.fillCity(data.user.city))
+
+        await test.step('Fill state field', async () =>
+            await accountCreationPage.fillState(randomize.state()))
+
+        await test.step('Fill postcode field', async () =>
+            await accountCreationPage.fillPostCode(data.user.postcode))
+
+        await test.step('Fill mobile phone field', async () =>
+            await accountCreationPage.fillMobilePhone(data.user.mobilephone))
+
+        await test.step('Click register button', async () =>
+            await accountCreationPage.clickRegisterButton())
+
+        await test.step('Validate "my account" title', async () =>
+            await myAccountPage.validateMyAccountTitle(data.titles.myaccountpage))
+
+        await test.step('Validate "my account" text', async () =>
+            await myAccountPage.validateMyAccountText(data.texts.myaccountpageH1))
+
+        await test.step('Validate random username', async () =>
+            await header.validateRandomUserName(data.user.firstname, data.user.lastname))
+
     });
 
     test('Unsuccessful user registration', async ({ page }) => {
-        await authenticationPage.fillEmail(process.env.EMAIL);
-        await authenticationPage.clickCreateAnAccountButton();
-        await authenticationPage.validateRegisteredEmailError(data.errors.registerEmailErrorMessage);
+
+        await test.step('Fill email field', async () =>
+            await authenticationPage.fillEmail(process.env.EMAIL))
+
+        await test.step('Click "create an account" button', async () =>
+            await authenticationPage.clickCreateAnAccountButton())
+
+        await test.step('Validate registered email error message', async () =>
+            await authenticationPage.validateRegisteredEmailError(data.errors.registerEmailErrorMessage))
+
     });
 
 });
